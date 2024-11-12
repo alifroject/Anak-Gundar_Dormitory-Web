a492010ed248899d458fa1f8a3b4acebaa9e1602
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2024-11-12 09:31:17 +0700

    clear2

--- src/app/api/midtransToken/route.ts
+++ src/app/api/midtransToken/route.ts
@@ -24,26 +24,30 @@ export async function POST(req: Request) {
     }
 
     // Handle POST request for transaction
-    const { orderId, grossAmount, customerDetails }: TransactionRequestBody = await req.json();
+    if (req.method === 'POST') {
+        const { orderId, grossAmount, customerDetails }: TransactionRequestBody = await req.json();
 
-    const snap = new midtransClient.Snap({
-        isProduction: false,
-        serverKey: process.env.MIDTRANS_SERVER_KEY || '',
-    });
+        const snap = new midtransClient.Snap({
+            isProduction: false,
+            serverKey: process.env.MIDTRANS_SERVER_KEY || '',
+        });
 
-    const transactionDetails = {
-        transaction_details: {
-            order_id: orderId,
-            gross_amount: grossAmount,
-        },
-        customer_details: customerDetails,
-    };
+        const transactionDetails = {
+            transaction_details: {
+                order_id: orderId,
+                gross_amount: grossAmount,
+            },
+            customer_details: customerDetails,
+        };
 
-    try {
-        const transaction = await snap.createTransaction(transactionDetails);
-        return NextResponse.json(transaction, { status: 200 });
-    } catch (error) {
-        const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-        return NextResponse.json({ error: errorMessage }, { status: 500 });
+        try {
+            const transaction = await snap.createTransaction(transactionDetails);
+            return NextResponse.json(transaction, { status: 200 });
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+            return NextResponse.json({ error: errorMessage }, { status: 500 });
+        }
+    } else {
+        return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });
     }
 }
